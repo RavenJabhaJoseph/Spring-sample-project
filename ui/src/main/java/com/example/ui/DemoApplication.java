705cce88ec0387388dbb8929b2f4cbb5e83ade75
RavenJabhaJoseph <ravenjabhajoseph@gmail.com>
2021-09-12 18:35:03 +0530

    Build successful RESTful simple WEB application
    learned the main @SpringBootApplication annotation and understanding fundamentals of Spring boot.

--- ui/src/main/java/com/example/ui/DemoApplication.java
+++ ui/src/main/java/com/example/ui/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.ui;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class,args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Lets inspect beans");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
 }
